ace80cf93ff21d52f237cf56e09c5e95c4fb4884
Lukas <lukas@fruntke.tech>
2020-03-25 11:22:06 +0100

    Introduction text for the website

--- src/home/home.component.js
+++ src/home/home.component.js
@@ -10,8 +10,15 @@ export default function Home() {
             <Row >
                 <Col >
                     <h1 className="text-center">
-                        DJ Sets #StayatHome
+                        DJ Sets
                     </h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <p className="text-center my-0">During the ongoing Corona pandemic, I find it relaxing to listen to good DJ sets and music.</p>
+                    <p className="text-center my-1">I collected sets from all over Techno, EDM, Dubstep, ... from October 2019 to today</p>
+                    <p className="text-center my-1">Cheers, Lukas</p>
                 </Col>
             </Row>
             <SetDisplayComponent/>
