da72c8a1e85f5b989630815978f41f1ce46cc0ff
Lukas <lukas@fruntke.tech>
2020-04-08 17:06:47 +0200

    Convert Set-detail to a function

--- src/sets/set-detail.component.js
+++ src/sets/set-detail.component.js
@@ -1,33 +1,29 @@
 import {Card} from "react-bootstrap";
-import React, {Component} from "react";
+import React from "react";
 import Img from 'react-image'
 
 
-
-
-export class SetDetailComponent extends Component {
-
-    render() {
-        let convertLinkToThumb = (link) => `${(link.replace('https://youtu.be/', 'https://img.youtube.com/vi/')).replace(/([?&]t)=[^?&]+/, "")}/0.jpg`;
-        let addTimestamp = (link) => link.includes("?t=") ? link : `${link}?t=1`;
-        return (
-            <Card className="mx-4 mx-lg-2">
-                <a href={addTimestamp(this.props.set.link)} target="_blank" rel="noopener noreferrer">
-                    <Img src={[convertLinkToThumb(this.props.set.link)]} className="card-img-top"/>
+export function SetDetailComponent(props) {
+    let convertLinkToThumb = (link) => `${(link.replace('https://youtu.be/', 'https://img.youtube.com/vi/')).replace(/([?&]t)=[^?&]+/, "")}/0.jpg`;
+    let addTimestamp = (link) => link.includes("?t=") ? link : `${link}?t=1`;
+    let {link, title, venue, artists, genre} = props.set;
+    return (
+        <Card className="mx-4 mx-lg-2" key={Object.values(props.set).join(',') + "-" + props.selectedGenre}>
+                <a href={addTimestamp(link)} target="_blank" rel="noopener noreferrer">
+                    <Img src={[convertLinkToThumb(link)]} className="card-img-top"/>
 
                     <Card.Body>
-                        <Card.Title>{this.props.set.title} {this.props.set.title && this.props.set.venue ? `@ ${this.props.set.venue}` : this.props.set.venue}</Card.Title>
+                        <Card.Title>{title} {title && venue ? `@ ${venue}` : venue}</Card.Title>
                         <Card.Text>
-                            <b>{this.props.set.artists.join(', ')}</b>
+                            <b>{artists.join(', ')}</b>
                         </Card.Text>
                     </Card.Body>
-                    <Card.Footer style={{height: "100%"}}>
+                    <Card.Footer>
                         <b>
-                            {this.props.set.genre}
+                            {genre}
                         </b>
                     </Card.Footer>
                 </a>
             </Card>
-        )
-    }
+    )
 }
